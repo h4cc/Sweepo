9e9953fc961447d3737ef5891a8a50948bf3d7eb
Rémy GAZELOT <r.gazelot@gmail.com>
2013-03-17 18:58:12 +0100

    Add test suite phpunit

---
+++ src/Sweepo/StreamBundle/Tests/Service/AnalyseTweetTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sweepo\StreamBundle\Tests\Service;
+
+class AnalyseTweetTest extends \PHPUnit_Framework_TestCase
+{
+    public function testA()
+    {
+        $this->assertTrue(true, 'message');
+    }
+}
