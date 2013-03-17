93168904d640ce0d9f8afd4da3ebc3d52bf36ef9
Rémy GAZELOT <r.gazelot@gmail.com>
2013-03-17 20:32:18 +0100

    Add CSS and architecture modifications

--- web/front/js/CoreBundle/layout.js
+++ web/front/js/CoreBundle/layout.js
@@ -1,9 +1,2 @@
 $(function() {
-    $('#disconnect_toggle').on('click', function() {
-        var self = $(this);
-        self.hide();
-        $('#disconnect, #user_edit').show().delay(4000).fadeOut(function() {
-            self.show();
-        });
-    });
 });
