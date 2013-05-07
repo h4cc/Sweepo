67a4b7b2245f102c9180948d7f8cb919ce549823
Rémy GAZELOT <r.gazelot@gmail.com>
2013-05-07 17:15:18 +0200

    Fix UT AnalyseTweet

--- src/Sweepo/StreamBundle/Tests/Service/AnalyseTweetTest.php
+++ src/Sweepo/StreamBundle/Tests/Service/AnalyseTweetTest.php
@@ -8,33 +8,56 @@ use Symfony\Component\Translation\TranslatorInterface;
 
 use Sweepo\StreamBundle\Entity\Subscription;
 use Sweepo\StreamBundle\Service\AnalyseTweet;
+use Sweepo\StreamBundle\Service\CreateTweet;
 
 class AnalyseTweetTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @dataProvider provider
+     * @dataProvider providerIsSubscribed
      */
     public function testIsSubscribed($subscriptions, $tweet, $exepected)
     {
-        // $translator = $this->getMockBuilder('Symfony\Component\Translation\TranslatorInterface')
-        //              ->disableOriginalConstructor()
-        //              ->getMock();
+        $createTweet = $this->getMockBuilder('Sweepo\StreamBundle\Service\CreateTweet')
+                     ->disableOriginalConstructor()
+                     ->getMock();
 
-        $analyse = new AnalyseTweet();
+        $analyse = new AnalyseTweet($createTweet);
         $answer = $analyse->isSubscribed($subscriptions, $tweet);
 
-        $this->assertEquals($exepected, $answer);
+        $this->assertInstanceOf('Sweepo\StreamBundle\Entity\Subscription', $answer);
+        $this->assertEquals($subscriptions[0]->getSubscription(), $answer->getSubscription());
     }
 
-    public function provider()
+    public function providerIsSubscribed()
     {
         return [
             // Test that we find the user type subscription 'foo'
             [[$this->addSubscription('@foo', '@foo')], $this->addTweetFromTwitter('foo', 'bar'), true],
-            // Test that we don't find subscription
-            [[$this->addSubscription('@foo', '@foo')], $this->addTweetFromTwitter('bar', 'bar'), false],
             // Test we find keyword subscription
             [[$this->addSubscription('bar', 'bar')], $this->addTweetFromTwitter('baz', 'The Bar is better than Foo'), true],
+        ];
+    }
+
+    /**
+     * @dataProvider providerIsNotSubscribed
+     */
+    public function testIsNotSubscribed($subscriptions, $tweet)
+    {
+        $createTweet = $this->getMockBuilder('Sweepo\StreamBundle\Service\CreateTweet')
+                     ->disableOriginalConstructor()
+                     ->getMock();
+
+        $analyse = new AnalyseTweet($createTweet);
+        $answer = $analyse->isSubscribed($subscriptions, $tweet);
+
+        $this->assertFalse($answer);
+    }
+
+    public function providerIsNotSubscribed()
+    {
+        return [
+            // Test that we don't find subscription
+            [[$this->addSubscription('@foo', '@foo')], $this->addTweetFromTwitter('bar', 'bar'), false],
             // Test we don't find keyword subscription
             [[$this->addSubscription('bar', 'bar')], $this->addTweetFromTwitter('foo', 'The Foo is better than Baz'), false],
         ];
